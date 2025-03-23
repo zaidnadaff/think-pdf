31826aa8fe94ffdf90770627e054da5a05bb60a0
zaid <zaidnadaf0@gmail.com>
2025-03-23 21:57:04 +0530

    auth sorted

--- backend/app/models/refreshToken.model.js
+++ backend/app/models/refreshToken.model.js
@@ -1,26 +1,29 @@
 const sequelize = require("../config/db.config.js");
+
 const { DataTypes } = require("sequelize");
+
 const { User } = require("../models/user.model.js");
 
 const RefreshToken = sequelize.define(
-  "RefreshToken",
+  "refreshToken",
+
   {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
+
     token: { type: DataTypes.STRING, allowNull: false },
+
     expiresAt: { type: DataTypes.DATE, allowNull: false },
   },
+
   { timestamps: true }
 );
 
-RefreshToken.associate = (models) => {
-  RefreshToken.belongsTo(models.User, {
-    foreignKey: "userId",
-    onDelete: "CASCADE",
-  });
-};
+User.hasMany(RefreshToken);
+
+RefreshToken.belongsTo(User);
 
 module.exports = { RefreshToken };
--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -1,47 +1,48 @@
 const sequelize = require("../config/db.config.js");
+
 const { DataTypes } = require("sequelize");
+
 const bcrypt = require("bcrypt");
 
 const User = sequelize.define(
-  "User",
+  "user",
+
   {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
+
     name: { type: DataTypes.STRING, allowNull: false },
+
     email: {
       type: DataTypes.STRING,
+
       allowNull: false,
+
       unique: true,
+
       validate: {
         isEmail: true,
       },
     },
+
     password: { type: DataTypes.STRING, allowNull: false },
   },
+
   {
     timestamps: true,
+
     hooks: {
       beforeCreate: async (user) => {
         const salt = await bcrypt.genSalt(10);
+
         user.password = await bcrypt.hash(user.password, salt);
       },
     },
   }
 );
-
-User.associate = (models) => {
-  User.hasMany(models.RefreshToken, {
-    foreignKey: "userId",
-    as: "refreshTokens",
-  });
-  User.hasMany(models.Document, {
-    foreignKey: "userId",
-    as: "documents",
-  });
-};
 
 User.prototype.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
